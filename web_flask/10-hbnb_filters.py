c2106adadc2b95f9c6a1c74919d8770aa218d705
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-03-10 04:47:23 -0800

    task 10

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,27 +4,31 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /hbnb_filters: HBnB HTML filters page.
 """
-from models import storage
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
-
-
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_func(self):
+    """Will excecute after each request
+    Closes current sqlalchemy session
+    """
     storage.close()
 
 
+@app.route('/hbnb_filters')
+def copyHTML():
+    """display a HTML page like 6-index.html"""
+    state = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=state, amenities=amenities)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
